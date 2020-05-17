f01722de06b791557743a552f9d4dd415e776427
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-05-17 12:37:24 +0100

    Countdown timer displaying and working

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,10 +8,30 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+int s = 3666 ;// number of second we want to start counting from
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::Timer1Timer(TObject *Sender)
+{
+     //this code will be executed every  1000 miliseconds = 1sec  due to Interval property 1000
+     s--;
+
+     int hour,min,sec;
+     AnsiString godziny,minuty,sekundy; // we need it to display so need to convert 'ints' for 'strings'
+
+     hour = s / 3600;
+     min = ( s - hour * 3600) / 60;
+     sec =  ( s - hour * 3600) - min * 60;
+
+     godziny = IntToStr(hour);
+     minuty =  IntToStr(min);
+     sekundy = IntToStr(sec);
+
+     Label1->Caption = godziny+":"+minuty+":"+sekundy;
+}
+//---------------------------------------------------------------------------
--- Unit1.dfm
+++ Unit1.dfm
@@ -13,4 +13,23 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 48
+    Top = 144
+    Width = 681
+    Height = 41
+    Alignment = taCenter
+    AutoSize = False
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -32
+    Font.Name = 'MS Sans Serif'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Timer1: TTimer
+    OnTimer = Timer1Timer
+    Left = 120
+    Top = 32
+  end
 end
